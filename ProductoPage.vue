16a9d428d35cd67d90f6d9b7f5e4fbea598b1b6d
Oscar29400 <77684795+Oscar29400@users.noreply.github.com>
2022-05-08 19:58:48 +0200

    Add files via upload

---
+++ ProductoPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <q-page>
+    <div class="row">
+      
+    </div>
+  </q-page>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default {
+  name: "PageProductos",
+  data() {
+    return {
+      
+    };
+  },
+  mounted() {
+    this.getProductos();
+  },
+  methods: {
+    getProductos() {
+      this.$axios
+        .get("http://localhost:8069/gestion/cargamento/productos")
+        .then((res) => {
+          this.rows = res.data;
+          var image = new Image(200, 200);
+          image.src = this.rows[0].img;
+          this.rows[0].foto = image;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    goTo(event, row) {
+      this.$router.push("/producto/" + row.id);
+    },
+  },
+};
+</script>
